d1dbe7c96962904ec103a597192387496f4b8ddf
Code1337 <35568583+D3codeV09@users.noreply.github.com>
2020-09-04 14:33:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+    
+(function($){
+  
+    $(".menu-icon").on("click", function(){
+          $(this).toggleClass("open");
+          $(".container").toggleClass("nav-open");
+          $("nav ul li").toggleClass("animate");
+    });
+    
+  })(jQuery);
